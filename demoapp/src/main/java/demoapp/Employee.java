27cef155a7d46bf40c7487249bce7ca59cb0105e
capgteam <capgteam22020@gmail.com>
2021-07-08 09:24:51 +0530

    Employee class added with getter and setter

--- demoapp/src/main/java/demoapp/Employee.java
+++ demoapp/src/main/java/demoapp/Employee.java
@@ -3,5 +3,20 @@ package demoapp;
 public class Employee {
 	int empId;
 	String empName;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	
+	
+	
 
 }
