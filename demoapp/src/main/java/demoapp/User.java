b8d380aa72ceccfc7db9ff93a76ef067f2d20fb2
capgteam <capgteam22020@gmail.com>
2021-07-08 12:07:05 +0530

    calculate metod added

--- demoapp/src/main/java/demoapp/User.java
+++ demoapp/src/main/java/demoapp/User.java
@@ -3,5 +3,12 @@ package demoapp;
 public class User {
 	String userName;
 	String userPassword;
+	
+	public void calculate() {
+		int[] arr= {1,2,3,4,5,6};
+		int sum=0;
+		for(int i=0;i<arr.length;i++)
+			sum+=arr[i];
+	}
 
 }
